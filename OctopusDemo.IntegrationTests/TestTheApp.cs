d3ea676f8aa43a77d1eace51440062a4d6413d98
Sean Fitzgerald <sean.fitzgerald@consultant.renre.com>
2016-06-08 15:39:17 +0100

    Change namespace

--- OctopusDemo.IntegrationTests/TestTheApp.cs
+++ OctopusDemo.IntegrationTests/TestTheApp.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 
-namespace OctopusDemo.IntegrationsTests
+namespace OctopusDemo.IntegrationTests
 {
 	[TestFixture]
     public class TestTheApp
