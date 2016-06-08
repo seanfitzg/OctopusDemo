ad0eed0ec3e0c1985b0f141d5121393ec27d221e
Sean Fitzgerald <sean.fitzgerald@consultant.renre.com>
2016-06-08 15:23:02 +0100

    Add integration tests

---
+++ OctopusDemo.IntegrationsTests/TestTheApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace OctopusDemo.IntegrationsTests
+{
+	[TestFixture]
+    public class TestTheApp
+    {
+		[Test]
+		public void RunATest()
+		{
+			Assert.That(1, Is.EqualTo(1));
+		}
+    }
+}
